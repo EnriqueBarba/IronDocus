c8f4232920ccce16e1633d2dc17b7aa9fa233162
EnriqueBarba <enrique.barbadillo26@gmail.com>
2019-12-12 21:06:36 +0100

    feature/document_logic_1 first commit testing

---
+++ controllers/cat.controller.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Cat = require('../models/category.model');
+const Docu = require('../models/document.model');
+const User = require('../models/user.model');
+const Depart = require('../models/depart.model');
+
+module.exports.findCategories = (req, res, next) => {
+    return "HOLA"
+}
