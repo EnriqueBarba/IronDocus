1994e249c7c9fa774ef2584d39963ce5c70406de
root <root@LAPTOP-TCKDMSCV.localdomain>
2019-12-14 13:07:11 +0100

    fix/categories_view added categories_view

--- controllers/doc.controller.js
+++ controllers/doc.controller.js
@@ -6,8 +6,8 @@ const Cat = require('../models/category.model');
 const mailer = require('../config/mailer.config');
 
 module.exports.new = (req, res, next) => {
-    const doc = new Docu();  
-    res.render('docs/form', {doc})
+    const doc = new Docu();
+    res.render('docs/form', { doc })
 }
 
 module.exports.doNew = (req, res, next) => {
@@ -16,4 +16,26 @@ module.exports.doNew = (req, res, next) => {
 
 module.exports.edit = (req, res, next) => {
     res.redirect('/')
+}
+
+module.exports.findByCat = (req, res, next) => {
+    const catFlag = req.params.catFlag
+    console.log(catFlag)
+    Cat.findOne({ flag: catFlag })
+        .then(cat => {
+            console.log(cat)
+            if (cat) {
+                Docu.find({ category: cat._id })
+                    .populate('author')
+                    .populate('category')
+                    .sort({ createdAt: -1 })
+                    .then(docs => {
+                        res.render('cat/cat', { docs })
+                    })
+                    .catch(next)
+            }
+        })
+
+        .catch(next)
+
 }
